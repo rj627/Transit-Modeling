56438ea7a9b54a7ae8526243f3011407e13661c3
Rahul Jayaraman <rahuljayaraman@Rahuls-MacBook-Pro.local>
2017-12-13 11:20:57 -0500

    modified data script

--- get_data.py
+++ get_data.py
@@ -4,17 +4,19 @@ import sys
 import os
 
 def main(plnm, aor, ramp):
-	print (os.getcwd())
 	data = np.load('../'+plnm + '/' + aor + '/apr_fits/' + ramp + '/' + aor + '_mcmc_results.npz')
 	print (str(plnm) + ', ' + str(aor) + ', with ramp ' + str(ramp))
-	print('T0: ' + str(data['t0_mcmc']))
-	print('Rp: ' + str(data['rp_mcmc']))
+	print('\n')
 	print('BIC: ' + str(data['bic']))
+	print('\n')
+	print('T_0: ' + str(data['t0_mcmc']))
+	print('R_p: ' + str(data['rp_mcmc']))
+	print('\n')
 	if (ramp=='exp'):
-		print ('a1: ' + str(data['a1_mcmc']))
-		print ('a2: ' + str(data['a2_mcmc']))
+		print ('a_1: ' + str(data['a1_mcmc']))
+		print ('a_2: ' + str(data['a2_mcmc']))
 	elif (ramp=='slant'):
-		print ('a1: ' + str(data['a1_mcmc']))
+		print ('a_1: ' + str(data['a1_mcmc']))
 
 	print('\n\n\n\n')
 
